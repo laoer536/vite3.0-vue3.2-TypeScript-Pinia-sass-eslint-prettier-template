e9c9c8b7cb5d9cd37f6d91d7f347b3a87131fc1b
laoer536 <1547698569@qq.com>
2022-11-17 00:57:51 +0800

    feat: nprogress加载加入

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,19 @@
 // vite-env.d.ts
 /// <reference types="vite-plugin-pages/client" />
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import NProgress from 'nprogress'
 import routes from '~pages'
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH as string),
   routes,
+})
+//nprogress
+router.beforeEach((to, from) => {
+  if (to.path !== from.path) NProgress.start()
+})
+router.afterEach(() => {
+  NProgress.done()
 })
 export default router
