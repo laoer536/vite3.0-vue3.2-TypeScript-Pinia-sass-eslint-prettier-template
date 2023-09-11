50fd5862a66bf7587a708aa201eb13f987a383ec
laoer536 <1547698569@qq.com>
2023-09-11 23:24:35 +0800

    perf: axios

--- src/network/axios/index.ts
+++ src/network/axios/index.ts
@@ -92,16 +92,16 @@ class MyAxios {
     return this.axiosInstance.get(url, { params: data })
   }
 
-  post<T>(url: string, data?: object): Promise<T> {
-    return this.axiosInstance.post(url, data)
+  post<T>(url: string, data?: object, params?: object): Promise<T> {
+    return this.axiosInstance.post(url, data, { params })
   }
 
-  put<T>(url: string, data?: object): Promise<T> {
-    return this.axiosInstance.put(url, data)
+  put<T>(url: string, data?: object, params?: object): Promise<T> {
+    return this.axiosInstance.put(url, data, { params })
   }
 
   delete<T>(url: string, data?: object): Promise<T> {
-    return this.axiosInstance.delete(url, data)
+    return this.axiosInstance.delete(url, { params: data })
   }
 
   upload<T>(data: Upload): Promise<T> {
@@ -115,7 +115,7 @@ class MyAxios {
 
   async uploadStream<T>(data: UploadStream): Promise<T> {
     const { url, file, controller, onUploadProgress } = data
-    /** generateSHA 生成文件SHA hash  用于标识文件唯一性 往往会用上 这里会用到crypto-js库 **/
+    /** generateSHA 生成文件SHA256 hash  用于标识文件唯一性 往往会用上 这里会用到crypto-js库 **/
     // async function generateSHA(file: File): Promise<string> {
     //   const wordArray = CryptoJs.lib.WordArray.create(await file.arrayBuffer())
     //   const sha256 = CryptoJs.SHA256(wordArray)
@@ -131,30 +131,37 @@ class MyAxios {
     })
   }
 
-  axiosDownload(params: AxiosDownload): void {
+  axiosDownload(params: AxiosDownload): Promise<{ fileName: string }> {
     const { url, data, controller, fileName, onDownloadProgress } = params
-    this.axiosInstance
-      .get<Blob>(url, {
-        params: data,
-        responseType: 'blob',
-        onDownloadProgress,
-        signal: controller ? controller.signal : undefined, //用于文件下载可以取消  只需在外部调用controller.abort()即可。 参考//https://juejin.cn/post/6954919023205154824以及https://axios-http.com/zh/docs/cancellation
-      })
-      .then((res) => {
-        const blob = new Blob([res.data])
-        const a = document.createElement('a')
-        a.style.display = 'none'
-        if (fileName) {
-          a.download = fileName
-        } else {
-          a.download = decodeURIComponent(analysisFilename(res.headers['content-disposition']))
-        }
-        a.href = URL.createObjectURL(blob)
-        document.body.appendChild(a)
-        a.click()
-        URL.revokeObjectURL(a.href)
-        document.body.removeChild(a)
-      })
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .get<Blob>(url, {
+          params: data,
+          responseType: 'blob',
+          onDownloadProgress,
+          signal: controller ? controller.signal : undefined, //用于文件下载可以取消  只需在外部调用controller.abort()即可。 参考//https://juejin.cn/post/6954919023205154824以及https://axios-http.com/zh/docs/cancellation
+        })
+        .then((res) => {
+          const blob = new Blob([res.data])
+          const a = document.createElement('a')
+          a.style.display = 'none'
+          if (fileName) {
+            a.download = fileName
+          } else {
+            a.download = decodeURIComponent(analysisFilename(res.headers['content-disposition']))
+          }
+          a.href = URL.createObjectURL(blob)
+          document.body.appendChild(a)
+          const downloadFileName = a.download
+          a.click()
+          URL.revokeObjectURL(a.href)
+          document.body.removeChild(a)
+          resolve({ fileName: downloadFileName })
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
   }
 
   urlDownload(params: UrlDownload) {
